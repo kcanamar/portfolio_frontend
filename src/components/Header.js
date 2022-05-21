2041f694bb47d01b48b0a6345125e8711d83178b
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-21 12:10:57 -0700

    setup base for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom"
+
 export default function Header(props) {
-    return <h1> Header </h1>
+    const navStyle = {
+        display:"flex",
+        justifyContnet:"space-around",
+        border: "3px solid black",
+        padding:"8px",
+        width: "90%",
+        margin: "auto",
+    }
+
+    return (
+        <header>
+            <h1>My Portfolio Page</h1>
+            <nav style={navStyle}>
+                <Link to="/">HOME</Link>
+                <Link to="/about">ABOUT</Link>
+                <Link to="/projects">PROJECTS</Link>
+            </nav>
+        </header>
+    )
 }
